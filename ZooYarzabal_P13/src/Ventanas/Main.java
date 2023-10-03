6c21abcfaac20a0d1a851587e9615e24990dd0a9
unaio <unaio@LaptopUnai>
2023-10-03 21:13:33 +0200

    a

--- ZooYarzabal_P13/src/Ventanas/Main.java
+++ ZooYarzabal_P13/src/Ventanas/Main.java
@@ -1,10 +1,37 @@
 package Ventanas;
 
-public class Main {
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+
+import javax.swing.*;
+
+public class Main extends JFrame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Propiedades principales de la ventana principal
+		Main ventanaPrincipal = new Main();
+		ventanaPrincipal.setVisible(true);
+		ventanaPrincipal.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		ventanaPrincipal.setSize(800, 600);
+		ventanaPrincipal.setTitle("ZOOyarzabal");
 
+	}
+	
+	// Aquí ya empezamos a construir
+	public Main() {
+		JPanel panelImagen = new JPanel();
+		panelImagen.setBackground(Color.BLUE); // Hasta que pongamos la imagen lo dejamos así
+		add(panelImagen, BorderLayout.CENTER);
+		
+		JPanel panelBotones = new JPanel();
+		panelBotones.setLayout(new FlowLayout());
+		// Aquí creamos el botón de trabajador y usuario
+		JButton botonTrabajador = new JButton("Soy trabajador");
+		panelBotones.add(botonTrabajador);
+		JButton botonVisitante = new JButton("Soy visitante");
+		panelBotones.add(botonVisitante);
+		add(panelBotones, BorderLayout.SOUTH);
 	}
 
 }
--- ZooYarzabal_P13/src/module-info.java
+++ ZooYarzabal_P13/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module ZooYarzabal_P13 {
+	requires java.desktop;
 }
