311a9f55ad375022cb0e17adb280b75e32df9867
ikerariasmartinez <ikerariasmartinez@localhost>
2023-10-23 10:34:17 +0200

    Ventana Veterinario

---
+++ ZooYarzabal_P13/src/Ventanas/VentanaVeterinario.java
@@ -0,0 +1,16 @@
+package Ventanas;
+
+import java.util.*;
+import javax.swing.*;
+import Zoo.Animal;
+
+public class VentanaVeterinario extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private ArrayList<Animal> animales;
+
+}
