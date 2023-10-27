65f471dd90e8c9e8046a954730010400c6bfeb68
ikerariasmartinez <ikerariasmartinez@localhost>
2023-10-27 10:12:31 +0200

    Cambios UNAI

--- ZooYarzabal_P13/src/Zoo/ZOO.java
+++ ZooYarzabal_P13/src/Zoo/ZOO.java
@@ -3,34 +3,73 @@ package Zoo;
 import java.util.ArrayList;
 
 public class ZOO {
-	
-	protected ArrayList<Animal> animales;
-	protected ArrayList<Trabajador> trabajadores;
-	protected ArrayList<Visistante> visitantes;
-	public ArrayList<Animal> getAnimales() {
-		return animales;
-	}
-	public void setAnimales(ArrayList<Animal> animales) {
-		this.animales = animales;
-	}
-	public ArrayList<Trabajador> getTrabajadores() {
-		return trabajadores;
-	}
-	public void setTrabajadores(ArrayList<Trabajador> trabajadores) {
-		this.trabajadores = trabajadores;
-	}
-	public ArrayList<Visistante> getVisitantes() {
-		return visitantes;
-	}
-	public void setVisitantes(ArrayList<Visistante> visitantes) {
-		this.visitantes = visitantes;
-	}
-	
-	// Para probar las asignaciones en la VentanaVeterinario
-	public void anyadirAnimalesYVeterinarios() {
-		animales.add(new Animal("agote", 173, Genero.FEMENINO, 33, new Trabajador(), "kdajfkdj", false, false));
-		
-		trabajadores.add(new Trabajador(3, "akd", "ds", 2, 2, Puesto.VETERINARIO, 333, "dk"));
-	}
+
+
+protected ArrayList<Animal> animales;
+
+protected ArrayList<Trabajador> trabajadores;
+
+protected ArrayList<Visistante> visitantes;
+
+public ArrayList<Animal> getAnimales() {
+
+return animales;
+
+}
+
+public void setAnimales(ArrayList<Animal> animales) {
+
+this.animales = animales;
+
+}
+
+public ArrayList<Trabajador> getTrabajadores() {
+
+return trabajadores;
+
+}
+
+public void setTrabajadores(ArrayList<Trabajador> trabajadores) {
+
+this.trabajadores = trabajadores;
+
+}
+
+public ArrayList<Visistante> getVisitantes() {
+
+return visitantes;
+
+}
+
+public void setVisitantes(ArrayList<Visistante> visitantes) {
+
+this.visitantes = visitantes;
+
+}
+
+public ZOO(ArrayList<Animal> animales, ArrayList<Trabajador> trabajadores, ArrayList<Visistante> visitantes) {
+
+super();
+
+this.animales = animales;
+
+this.trabajadores = trabajadores;
+
+this.visitantes = visitantes;
+
+}
+
+public ZOO() {
+
+super();
+
+this.animales = new ArrayList<>();
+
+this.trabajadores = new ArrayList<>();
+
+this.visitantes = new ArrayList<>();
+
+}
+
 
 }
