9a83e468c81ba60b2ea289a5399177dae00594aa
unaio <unaio@LaptopUnai>
2023-11-30 11:01:09 +0100

    a

--- ZooYarzabal_P13/src/Ventanas/Main.java
+++ ZooYarzabal_P13/src/Ventanas/Main.java
@@ -15,7 +15,7 @@ public class Main {
 	
 	public Main() {
 		
-		// Crear una ventana principal
+		// Crear una ventana principall
         ventana = new JFrame("Ventana Principal");
         ventana.setSize(800, 600);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
